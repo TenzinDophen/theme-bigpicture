0ae9bd7494c85850abe382c8d403d3f75b0ea1df
TenzinDophen <dophent@carleton.edu>
2018-02-07 17:20:08 -0600

    making the previouus and the next button of the search result respond according to the sorted order of the searched item

--- search/index.php
+++ search/index.php
@@ -7,31 +7,50 @@ $searchRecordTypes = get_search_record_types();
 <?php echo search_filters(); ?>
 
 <?php if ($total_results): ?>
-<div class="browse-header">
 <?php echo pagination_links(); ?>
-</div>
+<table id="search-results">
+    <thead>
+        <tr>
+            <th><?php echo __('Record Type');?></th>
+            <th><?php echo __('Title');?></th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php $filter = new Zend_Filter_Word_CamelCaseToDash; ?>
+        <?php foreach (loop('search_texts') as $searchText): ?>
+        <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
+        <?php $recordType = $searchText['record_type']; ?>
+        <?php set_current_record($recordType, $record); ?>
+        <tr class="<?php echo strtolower($filter->filter($recordType)); ?>">
+            <td>
+                <?php echo $searchRecordTypes[$recordType]; ?>
+            </td>
+            <td>
+                <?php if ($recordImage = record_image($recordType)): ?>
+                    <?php echo link_to($record, 'show', $recordImage, array('class' => 'image')); ?>
+                <?php endif; ?>
+                <?php
 
-<div class="records">
-    <?php $filter = new Zend_Filter_Word_CamelCaseToDash; ?>
-    <?php foreach (loop('search_texts') as $searchText): ?>
-    <?php $record = get_record_by_id($searchText['record_type'], $searchText['record_id']); ?>
-    <?php $recordType = $searchText['record_type']; ?>
-    <?php set_current_record($recordType, $record); ?>
-    <div class="<?php echo strtolower($filter->filter($recordType)); ?>">
-        <div class="record">
-            <?php if ($recordImage = record_image($recordType)): ?>
-                <?php echo link_to($record, 'show', $recordImage, array('class' => 'image')); ?>
-            <?php endif; ?>
-            <div class="record-type"><?php echo $searchRecordTypes[$recordType]; ?></div>
-            <a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a>
-        </div>
-    </div>
-    <?php endforeach; ?>
-    
-    <div class="browse-header">
-        <?php echo pagination_links(); ?>
-    </div>
-</div>
+        if(isset($_SERVER['QUERY_STRING']) && !empty($_SERVER['QUERY_STRING']))
+            {
+            $searchlink = record_url($record, 'show').'?' . $_SERVER['QUERY_STRING'];
+        ?>
+            <td><a href="<?php echo $searchlink; ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a></td>
+            <?php
+            }
+        else
+            {
+            ?>
+            <td><a href="<?php echo record_url($record, 'show'); ?>"><?php echo $searchText['title'] ? $searchText['title'] : '[Unknown]'; ?></a></td>
+            <?php
+            }
+        ?>
+            </td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
+<?php echo pagination_links(); ?>
 <?php else: ?>
 <div id="no-results">
     <p><?php echo __('Your query returned no results.');?></p>
